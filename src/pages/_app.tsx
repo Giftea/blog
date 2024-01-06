b09a1be2de680d35754aa2292a22703fb20906e9
codev206 <zionekekenta@gmail.com>
2024-01-06 01:17:15 +0100

    [FEAT] add Google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { useEffect } from "react"
 import { RootLayout } from "src/layouts"
 import { queryClient } from "src/libs/react-query"
 import posthog from "posthog-js"
+import Head from "next/head"
 
 function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout || ((page) => page)
@@ -14,11 +15,28 @@ function App({ Component, pageProps }: AppPropsWithLayout) {
     posthog.capture("my event", { property: "value" })
   })
   return (
-    <QueryClientProvider client={queryClient}>
-      <Hydrate state={pageProps.dehydratedState}>
-        <RootLayout>{getLayout(<Component {...pageProps} />)}</RootLayout>
-      </Hydrate>
-    </QueryClientProvider>
+    <>
+      <Head>
+        {/* Add the Google Tag Manager script */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-8CZW3LFRXM"
+        ></script>
+        <script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-8CZW3LFRXM');
+          `}
+        </script>
+      </Head>
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <RootLayout>{getLayout(<Component {...pageProps} />)}</RootLayout>
+        </Hydrate>
+      </QueryClientProvider>
+    </>
   )
 }
 
